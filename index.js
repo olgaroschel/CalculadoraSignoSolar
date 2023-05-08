56471b6f3491bcb4786a3de84f831f4b676d72da
Olga Roschel <82820039+olgaroschel@users.noreply.github.com>
2023-05-08 12:37:16 +0100

    First deploy

---
+++ index.js
@@ -0,0 +1,42 @@
+function calcularSigno() {
+    let dia = parseInt(document.getElementById("dia").value);
+    let mes = document.getElementById("mes").value;
+    let resultado;
+
+    if ((dia >= 21 && mes === 'março') || (dia <= 20 && mes === 'abril')) {
+        resultado = "Áries";
+    } else if ((dia >= 21 && mes === 'abril') || (dia <= 20 && mes === 'maio')) {
+        resultado = "Touro";
+    } else if ((dia >= 21 && mes === 'maio') || (dia <= 20 && mes === 'junho')) {
+        resultado = "Gêmeos";
+    } else if ((dia >= 21 && mes === 'junho') || (dia <= 21 && mes === 'julho')) {
+        resultado = "Câncer";
+    } else if ((dia >= 22 && mes === 'julho') || (dia <= 22 && mes === 'agosto')) {
+        resultado = "Leão";
+    } else if ((dia >= 23 && mes === 'agosto') || (dia <= 22 && mes === 'setembro')) {
+        resultado = "Virgem";
+    } else if ((dia >= 23 && mes === 'setembro') || (dia <= 22 && mes === 'outubro')) {
+        resultado = "Libra";
+    } else if ((dia >= 23 && mes === 'outubro') || (dia <= 21 && mes === 'novembro')) {
+        resultado = "Escorpião";
+    } else if ((dia >= 22 && mes === 'novembro') || (dia <= 21 && mes === 'dezembro')) {
+        resultado = "Sagitário";
+    } else if ((dia >= 22 && mes === 'dezembro') || (dia <= 19 && mes === 'janeiro')) {
+        resultado = "Capricórnio";
+    } else if ((dia >= 20 && mes === 'janeiro') || (dia <= 18 && mes === 'fevereiro')) {
+        resultado = "Aquário";
+    } else if ((dia >= 19 && mes === 'fevereiro') || (dia <= 20 && mes === 'março')) {
+        resultado = "Peixes";
+    } else {
+        resultado = "Data inválida";
+    }
+
+    return resultado;
+}
+
+function mostrarResultado() {
+    const dia = document.getElementById("dia").value;
+    const mes = document.getElementById("mes").value;
+    const resultado = calcularSigno(dia, mes);
+    document.getElementById("resultado").textContent = "Seu signo é " + resultado + "!";
+}
